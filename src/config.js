a6bc2a1f8f2aa892cdaba1b5c611002fac826b25
or-else <or.else@gmail.com>
2019-08-22 10:46:14 +0300

    migrate the app from api.. to web..

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import { PACKAGE_VERSION } from './version.js';
 
 // Name of this application, used in the User-Agent.
-export const APP_NAME = 'TinodeWeb/' + (PACKAGE_VERSION || '0.15');
+export const APP_NAME = 'TinodeWeb/' + (PACKAGE_VERSION || '0.16');
 
 // API key. Use https://github.com/tinode/chat/tree/master/keygen to generate your own
 export const API_KEY = 'AQEAAAABAAD_rAp4DJh05a1HAwFT3A6K';
 
 // The array of possible hosts to connect to.
-export const KNOWN_HOSTS = {hosted: 'api.tinode.co', local: 'localhost:6060'};
+export const KNOWN_HOSTS = {hosted: 'web.tinode.co', local: 'localhost:6060'};
 
 // Default host name and port to connect to.
 export const DEFAULT_HOST = KNOWN_HOSTS.hosted;
