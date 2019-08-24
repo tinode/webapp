d9c729d9ab99951e7c9fcb54711d99bf8a8d55c1
or-else <or.else@gmail.com>
2019-08-24 14:31:55 +0300

    update dependencies and rebuild

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Must be located at the root.
-importScripts('https://unpkg.com/firebase@6.3.0/firebase-app.js');
-importScripts('https://unpkg.com/firebase@6.3.0/firebase-messaging.js');
+importScripts('https://unpkg.com/firebase@6.4.2/firebase-app.js');
+importScripts('https://unpkg.com/firebase@6.4.2/firebase-messaging.js');
 importScripts('firebase-init.js');
 
 firebase.initializeApp(FIREBASE_INIT);
