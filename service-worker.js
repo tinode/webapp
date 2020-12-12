1d1c9400f842fc33b8c1c1e238a14b0c7b0bae4c
or-else <or.else@gmail.com>
2020-12-11 17:19:01 -0800

    dependencies updated

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Must be located at the root.
-importScripts('https://cdn.jsdelivr.net/npm/firebase@7.20.0/firebase-app.js');
-importScripts('https://cdn.jsdelivr.net/npm/firebase@7.20.0/firebase-messaging.js');
+importScripts('https://cdn.jsdelivr.net/npm/firebase@7.24.0/firebase-app.js');
+importScripts('https://cdn.jsdelivr.net/npm/firebase@7.24.0/firebase-messaging.js');
 importScripts('firebase-init.js');
 
 firebase.initializeApp(FIREBASE_INIT);
