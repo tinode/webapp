9508797ab9886115700943d0dcd32b53b7dfc1aa
or-else <or.else@gmail.com>
2021-08-20 20:50:08 -0700

    added a "password reset email has been sent" screen

--- src/views/tinode-web.jsx
+++ src/views/tinode-web.jsx
@@ -1512,7 +1512,7 @@ class TinodeWeb extends React.Component {
 
   handlePasswordResetRequest(method, value) {
     // If already connected, connnect() will return a resolved promise.
-    this.tinode.connect()
+    return this.tinode.connect()
       .then(() => {
         return this.tinode.requestResetAuthSecret('basic', method, value);
       })
