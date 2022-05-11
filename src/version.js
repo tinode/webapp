b4a5ad121ba0539ef6554110ad00dcdcdcb20469
or-else <or.else@gmail.com>
2022-05-11 15:17:56 -0700

    bump version to 0.18.6 and rebuild

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // This is a generated file. Don't edit.
 
-export const PACKAGE_VERSION = "0.18.5";
+export const PACKAGE_VERSION = "0.18.6";
--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // This is a generated file. Don't edit.
 
-const PACKAGE_VERSION = "0.18.5";
+const PACKAGE_VERSION = "0.18.6";
