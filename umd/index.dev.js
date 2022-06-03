6dc91ad518226867e7293aad744d96294a6686f1
or-else <or.else@gmail.com>
2022-06-02 18:50:16 -0700

    bump version to 0.19.0-rc2

--- umd/index.dev.js
+++ umd/index.dev.js
@@ -3379,7 +3379,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "PACKAGE_VERSION": () => (/* binding */ PACKAGE_VERSION)
 /* harmony export */ });
-const PACKAGE_VERSION = "0.19.0-rc1";
+const PACKAGE_VERSION = "0.19.0-rc2";
 
 /***/ }),
 
