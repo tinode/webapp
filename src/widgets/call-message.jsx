f884874d43026141df0f21f54314c644c9b25bcc
or-else <or.else@gmail.com>
2022-06-09 11:29:33 -0700

    incorporate "missing" and "declined" call statuses

--- src/widgets/call-message.jsx
+++ src/widgets/call-message.jsx
@@ -7,17 +7,18 @@ import { secondsToTime } from '../lib/strformat.js';
 
 export default class CallMessage extends React.PureComponent {
   render() {
-    const isCallDropped = this.props.callState == 'disconnected';
+    const isCallDropped = ['declined', 'disconnected', 'missed'].includes(this.props.callState);
     const successClass = 'material-icons medium ' + (isCallDropped ? 'red' : 'green');
     const callIcon = this.props.incoming ?
       (isCallDropped ? 'call_missed' : 'call_received') :
       (isCallDropped ? 'call_missed_outgoing' : 'call_made');
     const duration = isCallDropped ? (
-        this.props.incoming ?
-        <FormattedMessage id="call_missed" defaultMessage="missed" description="Label for cancelled incoming call" /> :
+        this.props.incoming ? (this.props.callState == 'declined' ?
+          <FormattedMessage id="call_declined" defaultMessage="declined" description="Label for declined incoming call" /> :
+          <FormattedMessage id="call_missed" defaultMessage="missed" description="Label for missed incoming call" />
+        ) :
         <FormattedMessage id="call_cancelled" defaultMessage="cancelled" description="Label for cancelled outgoing call" />
-      ) :
-      <span>{secondsToTime(this.props.duration / 1000)}</span>;
+      ) : <span>{secondsToTime(this.props.duration / 1000)}</span>;
     return <div className="call-message">
       <div><i className="material-icons big gray">call</i></div>
       <div className="flex-column narrow">
--- src/widgets/call-status.jsx
+++ src/widgets/call-status.jsx
@@ -9,16 +9,17 @@ import { secondsToTime } from '../lib/strformat.js';
 //  success: true | false.
 export default class CallStatus extends React.PureComponent {
   render() {
-    const isCallDropped = this.props.callState == 'disconnected';
+    const isCallDropped = ['declined', 'disconnected', 'missed'].includes(this.props.callState);
     const icon2 = this.props.incoming ?
       (isCallDropped ? 'call_missed' : 'call_received') :
       (isCallDropped ? 'call_missed_outgoing' : 'call_made');
     const duration = isCallDropped ? (
-        this.props.incoming ?
-        <FormattedMessage id="call_missed" defaultMessage="missed" description="Label for cancelled incoming call" /> :
-        <FormattedMessage id="call_cancelled" defaultMessage="cancelled" description="Label for cancelled outgoing call" />
+      this.props.incoming ? (this.props.callState == 'declined' ?
+        <FormattedMessage id="call_declined" defaultMessage="declined" description="Label for declined incoming call" /> :
+        <FormattedMessage id="call_missed" defaultMessage="missed" description="Label for missed incoming call" />
       ) :
-      <span>{secondsToTime(this.props.duration / 1000)}</span>;
+      <FormattedMessage id="call_cancelled" defaultMessage="cancelled" description="Label for cancelled outgoing call" />
+    ) : <span>{secondsToTime(this.props.duration / 1000)}</span>;
     return (
       <>
         <div className="composed-material">
