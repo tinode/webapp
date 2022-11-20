7236c913da6c2dacde82f08efc1d92f0d8349e8b
or-else <or.else@gmail.com>
2022-11-20 13:36:08 -0800

    shorten edit preview

--- src/config.js
+++ src/config.js
@@ -98,7 +98,7 @@ export const QUOTED_REPLY_LENGTH = 30;
 // Length of a preview of a forwarded message.
 export const FORWARDED_PREVIEW_LENGTH = 84;
 // Length of preview for a message being edited.
-export const EDIT_PREVIEW_LENGTH = 60;
+export const EDIT_PREVIEW_LENGTH = 48;
 
 // Minimum duration of an audio recording in milliseconds (2 sec).
 export const MIN_DURATION = 2_000;
