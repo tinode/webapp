97208e14485b13e203c13c5667c98862bbbc16ac
or-else <or.else@gmail.com>
2022-12-30 12:26:46 -0800

    bum version to 0.20.4

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // This is a generated file. Don't edit.
 
-export const PACKAGE_VERSION = "0.20.3";
+export const PACKAGE_VERSION = "0.20.4";
--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // This is a generated file. Don't edit.
 
-const PACKAGE_VERSION = "0.20.3";
+const PACKAGE_VERSION = "0.20.4";
