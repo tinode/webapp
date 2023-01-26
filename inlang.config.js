c8e150721f647e933c5d1df31ba949f7ae197b86
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-01-27 00:26:57 +0100

    Create inlang.config.js

---
+++ inlang.config.js
@@ -0,0 +1,31 @@
+// https://inlang.com/documentation
+
+
+export async function defineConfig(env) {
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+  );
+
+  const pluginConfig = {
+    pathPattern: "./{language}.json",
+  };
+
+  return {
+    referenceLanguage: "en",
+    languages: [
+      "de", 
+      "en",
+      "es",
+      "fr",
+      "ko",
+      "ro",
+      "ru",
+      "zh-TW",
+      "zh"
+    ],
+    readResources: (args) =>
+      plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) =>
+      plugin.writeResources({ ...args, ...env, pluginConfig }),
+  };
+}
