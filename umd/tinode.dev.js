80d3f5336b389afc17aafa14f039cdd0a2342b93
or-else <or.else@gmail.com>
2025-07-09 15:22:38 +0300

    dependencies updated, rebuilt, version bumped to 24.1

--- umd/tinode.dev.js
+++ umd/tinode.dev.js
@@ -5378,7 +5378,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   PACKAGE_VERSION: () => (/* binding */ PACKAGE_VERSION)
 /* harmony export */ });
-const PACKAGE_VERSION = "0.24.0";
+const PACKAGE_VERSION = "0.24.1";
 
 /***/ })
 
--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // This is a generated file. Don't edit.
 
-const PACKAGE_VERSION = "0.24.0";
+const PACKAGE_VERSION = "0.24.1";
