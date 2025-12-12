b2bae67ed2e7166ff8794f8a48bb10e33745e651
or-else <or.else@gmail.com>
2025-12-12 09:13:31 +0300

    version bumped, dependencies updated

--- umd/tinode.dev.js
+++ umd/tinode.dev.js
@@ -5449,7 +5449,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   PACKAGE_VERSION: () => (/* binding */ PACKAGE_VERSION)
 /* harmony export */ });
-const PACKAGE_VERSION = "0.25.0-rc1";
+const PACKAGE_VERSION = "0.25.0";
 
 /***/ })
 
